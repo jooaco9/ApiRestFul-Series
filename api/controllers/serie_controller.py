ae68aa22d4912cfce39a720d82a7b114d67e6021
jooaco9 <joaquin.corbo9@gmail.com>
2025-05-17 23:19:12 -0300

    agregue una funcion nueva para obtener series a partir de cierto anio

--- api/controllers/serie_controller.py
+++ api/controllers/serie_controller.py
@@ -2,7 +2,9 @@ from sqlmodel import select
 from api import SerieModel
 
 # Controller de serie
-def get_series(db):
+
+# Funcion para obtener todas las series
+async def get_series(db):
     # Creo la consulta
     stmt = select(SerieModel)
 
@@ -10,3 +12,13 @@ def get_series(db):
     results = db.exec(stmt)
     series = results.all()
     return series
+
+# Funcion parra obtener las series estrenadas despues del "anio"
+async def get_series_anio(anio, db):
+    # Consulta para tomar las series mayor al "anio"
+    stmt = select(SerieModel).where(SerieModel.release_year >= anio)
+
+    # Obtengo las series
+    results = db.exec(stmt)
+    series_anio = results.all()
+    return series_anio
