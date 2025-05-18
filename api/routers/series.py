24beb43d88a445d666efde781b362376f6aa04c9
jooaco9 <joaquin.corbo9@gmail.com>
2025-05-17 23:19:55 -0300

    nuevo endpoint para obtener series a paritr de cierto anio

--- api/routers/series.py
+++ api/routers/series.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, Query, HTTPException
 from sqlalchemy.orm import Session
+from typing_extensions import Annotated
 
 # DB
 from api import get_db
@@ -13,6 +14,40 @@ from api import serie_controller
 # Enrutador para definir los endpoints
 router = APIRouter()
 
-@router.get("/", response_model=list[SerieModel], status_code=status.HTTP_200_OK)
+# Endopoint para obtener todas las series
+@router.get("/", response_model=list[SerieModel], status_code=status.HTTP_200_OK,
+            summary="Obtiene todas las series"
+            )
 async def get_series(db: Session = Depends(get_db)):
-    return serie_controller.get_series(db)
+    return await serie_controller.get_series(db)
+
+# Endpoint para obtener las series estrenadas despues de cierto año
+@router.get("/release_year", response_model=list[SerieModel], status_code=status.HTTP_200_OK,
+            summary="Obtiene series con filtro de años",
+            description="""
+                Devuelve todas las series las cuales se hayan estrenado despues del "anio" puesto como query parameter
+            """
+            )
+async def get_series_anio(anio: Annotated[int, Query(description="A partir de que año se estrenaron", gt=1980)],
+                          db: Session = Depends(get_db)):
+    series_anio = await serie_controller.get_series_anio(anio, db)
+
+    if not series_anio:
+        raise HTTPException(status_code=404, detail=f"No se encontraron series estrenadas despues del año {anio}")
+
+    return series_anio
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
