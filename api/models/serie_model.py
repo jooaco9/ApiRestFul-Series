c9e75125ce6cdd32f43c726d88f7c9cd96acac0d
jooaco9 <joaquin.corbo9@gmail.com>
2025-05-19 20:40:14 -0300

    modificacion del mapeo de la tabla de series

--- api/models/serie_model.py
+++ api/models/serie_model.py
@@ -9,3 +9,4 @@ class Serie(SQLModel, table=True):
     description: str = Field(description="Descripcion de la serie")
     release_year: int = Field(description="Anio de la fecha de salida de la serie")
     cover_url: str = Field(description="URL de la foto de la serie")
+    end_year: int = Field(description="Año en el que finalizo la serie, puede ser NULL si esta no está terminada")
