8541fc6e2e57e0c87a4ddf9498d0fd6745262200
jooaco9 <joaquin.corbo9@gmail.com>
2025-05-19 11:11:35 -0300

    se agrego imports para el manejo de generos

--- api/__init__.py
+++ api/__init__.py
@@ -6,12 +6,15 @@ from api.configdoc import tags_metadata
 
 # Modelos DB
 from api.models.serie_model import Serie as SerieModel
+from api.models.genre_model import Genre as GenreModel
 
 # Controladores
-from api.controllers import serie_controller
+from api.controllers import series_controller
+from api.controllers import genres_controller
 
 # Rutas
 from api.routers.series import router as serie_router
+from api.routers.genres import router as genre_router
 
 # App
 from api.main import app
