b3dcc02a7fb213ebf275828eb00b6c8367352d9b
jooaco9 <joaquin.corbo9@gmail.com>
2025-05-19 11:12:01 -0300

    configuracion para la documentacion de la ruta de generos

--- api/configdoc.py
+++ api/configdoc.py
@@ -3,5 +3,9 @@ tags_metadata = [
     {
         "name": "series",
         "description": "Operaciones con la tabla de series"
+    },
+    {
+        "name": "generos",
+        "description": "Operaciones con la tabla de generos"
     }
 ]
