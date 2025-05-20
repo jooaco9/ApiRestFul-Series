e1486c6c69a559d5de8df5d8d9cc04778bfa2570
jooaco9 <joaquin.corbo9@gmail.com>
2025-05-19 21:37:59 -0300

    modificacion en las funciones

--- api/controllers/series_controller.py
+++ api/controllers/series_controller.py
@@ -1,24 +1,43 @@
 from sqlmodel import select
-from api import SerieModel
+from api import SerieModel, SerieSchema, GenreModel, SeriesGenresModel
 
 # Controller de serie
 
 # Funcion para obtener todas las series
 async def get_series(db):
-    # Creo la consulta
-    stmt = select(SerieModel)
+    # Consulta para obtener todas las series
+    stmt = (
+        select(SerieModel, GenreModel.name)
+        .join(SeriesGenresModel, SerieModel.id == SeriesGenresModel.serie_id)
+        .join(GenreModel, SeriesGenresModel.genre_id == GenreModel.id)
+    )
+    results = db.exec(stmt).all()
 
-    # Listo las series
-    results = db.exec(stmt)
-    series = results.all()
-    return series
+    # Armo diccionario con cada serie con SerieSchema y agrego los generos
+    series_dict = {}
+    for serie, genre in results:
+        # Si la serie no esta la agrego con id como clave
+        if serie.id not in series_dict:
+            series_dict[serie.id] = SerieSchema(
+                title=serie.title,
+                description=serie.description,
+                release_year=serie.release_year,
+                cover_url=serie.cover_url,
+                end_year=serie.end_year,
+                genres=[]
+            )
+        # Agrego el genero a la serie
+        series_dict[serie.id].genres.append(genre)
+
+    # Retorno una lista pero de los valores del diccionario
+    return list(series_dict.values())
 
 # Funcion parra obtener las series estrenadas despues del "anio"
 async def get_series_anio(anio, db):
-    # Consulta para tomar las series mayor al "anio"
+    # Consulta para obtener las series mayor al "anio"
     stmt = select(SerieModel).where(SerieModel.release_year >= anio)
-
-    # Obtengo las series
     results = db.exec(stmt)
     series_anio = results.all()
-    return series_anio
+
+    # Transformar los resultados al esquema SerieSchema
+    return [SerieSchema(**serie.dict()) for serie in series_anio]
